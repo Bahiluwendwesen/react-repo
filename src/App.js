1b78c6298dec5fa222f7f7c09bee1e0c075cc3d4
Bahilu Wendwesen <1bahiluwendwesen@gmail.com>
2024-02-01 03:36:07 -0800

    ss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        last z11 my git  react app Edit <code>src/App.js</code> and save to reload.
+        last z11d my git  react app Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
